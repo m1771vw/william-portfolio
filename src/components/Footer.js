8183b50ddb2197c00e6d9b148b77b5685e4160ea
William Yang <williamyang93@gmail.com>
2019-01-06 13:31:35 -0800

    Updated blog and footer css with responsive padding

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import LogoGithub from 'react-ionicons/lib/LogoGithub';
 import LogoLinkedin from 'react-ionicons/lib/LogoLinkedin';
 const Footer = () => {
     return (
-        <footer className ="section-footer footer-text">
-            <div>©2018 WILLIAM YANG. ALL RIGHTS RESERVED.</div>
+        <footer id="footer-text" className ="section-footer">
+            <div className="footer-text">©2018 WILLIAM YANG. ALL RIGHTS RESERVED.</div>
             <div>
             <a href="https://github.com/m1771vw/">
                 <span><LogoGithub fontSize="2.0rem"/></span>
